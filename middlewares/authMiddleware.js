aa24e9955f39614c941674c8b786b75036f5a598
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2022-12-26 23:41:39 +0530

    updated authmiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -34,6 +34,44 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
+const mentorProtect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      // token looks like - Bearer <token> something
+      token = req.headers.authorization.split(" ")[1]; // we need token
+
+      // verify token
+      // JWT_SECRET = 'abcd1234'
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select("-password");
+    //   console.log(req.user)
+      if (req.user.role === 'Admin' || req.user.isMentor) {
+        next();
+      } 
+      else {
+        throw new Error("Access denied. Contact Mentor");
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error(error.message);
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not Authorized, no token");
+  }
+});
+
 const adminProtect = asyncHandler(async (req, res, next) => {
   let token;
 
@@ -72,4 +110,4 @@ const adminProtect = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = { protect, adminProtect };
+module.exports = { protect, adminProtect, mentorProtect };
--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getClasses, getClass, addClass, deleteClass, updateClass } = require('../controllers/classController')
-const { adminProtect, mentorProtect, protect } = require('../middlewares/authMiddleware')
+const { adminProtect, protect } = require('../middlewares/authMiddleware')
 
 router.route('/').get(mentorProtect, getClasses).post(mentorProtect, addClass)
 router.route('/:classId').delete(adminProtect, deleteClass).put(mentorProtect, updateClass).get(protect, getClass)
